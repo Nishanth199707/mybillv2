bb08288fa6b9d3c6d219f9dfa8a328b692ade92b
Nishanth199707 <nishsnth199707@gmail.com>
2025-01-08 10:19:11 +0530

    aaa

--- resources/views/business/ebillsettings.blade.php
+++ resources/views/business/ebillsettings.blade.php
@@ -14,8 +14,6 @@
                         <h5 class="mb-0">E-Way Bill Setup</h5>
                     </div>
                     <div class="card-body">
-                        <form method="POST" action="" enctype="multipart/form-data">
-                            @csrf
                             <div class="mb-3 row">
                                 <div class="col-md-3">
                                     <label class="form-label">Password</label>
@@ -24,9 +22,7 @@
                                     </div>
                                 </div>
                                 <div style="text-align: right;">
-                                   <button type="submit" class="btn btn-primary">Check</button>
-                                </div>
-                        </form>
+                        <button id="check_athu" class="btn btn-primary">Check</button>
                     </div>
                 </div>
             </div>
