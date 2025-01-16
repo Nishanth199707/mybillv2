668667f3e70f2ee55da7e70018b9c8558e542dca
Nishanth199707 <nishsnth199707@gmail.com>
2025-01-16 18:07:55 +0530

    return count

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gst_profile',225);
+            $table->json('gst_response');
             $table->rememberToken();
             $table->timestamps();
         });
